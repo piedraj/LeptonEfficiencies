5990d0b32533d556046793314a1e36962ba4d44d
piedraj <jonatan.piedra@gmail.com>
2020-05-06 12:02:44 +0200

    Submitting now to the right place

--- AnalysisMiniAODPhaseII/test/run.sh
+++ AnalysisMiniAODPhaseII/test/run.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
-#SBATCH --mail-user=piedra@ifca.unican.es  # email address
-#SBATCH --mail-type=ALL                    # Alerts sent when job begins, ends, or aborts
-#SBATCH --nodes=1                          # Number of nodes required
-#SBATCH --ntasks=1                         # Number of tasks required
-#SBATCH --mem=2G                           # Total memory (RAM) required, per node
-#SBATCH --time=1:00:00                     # Wall Clock time (dd-hh:mm:ss) [max of 14 days]
-#SBATCH --output=myjob.output              # Output and error messages go to this file
 
 # Additional information can be found in the Altamira Users Guide
 # https://confluence.ifca.es/display/IC/Altamira+Users+Guide#AltamiraUsersGuide-Batchsystem
-
-printf "\n [run.sh] Load PERL module\n"
-
-module load PERL
 
 printf "\n [run.sh] Setup CMS environment\n"
 
@@ -20,7 +9,7 @@ source /cvmfs/cms.cern.ch/cmsset_default.sh
 
 cd /gpfs/users/piedra/project/work/CMSSW_9_1_1_patch1/src
 
-printf "\n [run.sh] We are in the CMSSW release %s\n\n" $PWD
+printf "\n [run.sh] We are in the CMSSW release %s\n" $PWD
 
 eval `scramv1 runtime -sh`
 
--- AnalysisMiniAODPhaseII/test/submit.sh
+++ AnalysisMiniAODPhaseII/test/submit.sh
@@ -2,7 +2,11 @@
 
 # Following https://indico.ifca.es/event/1353/contributions/6553/attachments/1068/1420/Tier2.pdf
 
-sbatch run.sh -o myoutput.txt -e myerror.err -q qos
+# qos = quality of service
+# gridui_sort   is for (up to)  3 hours jobs
+# gridui_medium is for (up to) 24 hours jobs
+
+sbatch -o logfile.log -e errofile.err --qos=gridui_sort --partition=cloudcms run.sh
 
 squeue
 
